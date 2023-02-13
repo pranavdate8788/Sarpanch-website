8019d7dc356411255217a2bf7c9cd93ce0ca8ea0
pranav date <pranavdate908@gmail.com>
2023-02-13 22:02:27 +0530

    new slider added

--- script.js
+++ script.js
@@ -9,80 +9,17 @@ menubars.addEventListener("click", () => {
 menuclose.addEventListener("click", () => {
   sidemenu[0].classList.add("hideSidemenu");
 });
+ 
+var counter = 1;
+setInterval(function()  {
+  document.getElementById('radio'+ counter).checked = true;
+  counter++; 
 
-let currentSlide = 0;
-
-function startSlider() {
-  let imageCount = document.querySelectorAll("img");
-
-  if (imageCount.length === 0) {
-    imageCount = document.querySelectorAll("img");
-    images.style.transform = `translateX(0px)`;
-    return;
+  if(counter>4){
+    counter=1
   }
 
-  let images = document.querySelector("ul");
-  images.style.transform = `translateX(-${currentSlide * 650}px)`;
-
-  if (currentSlide === imageCount.length - 1) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-}
-
-setInterval(() => {
-  startSlider();
-}, 4000);
-
-let Slide = 0;
-
-function startSlider() {
-  let imageCount = document.querySelectorAll("#image0");
-
-  if (imageCount.length === 0) {
-    imageCount = document.querySelectorAll("#image0");
-    images.style.transform = `translateX(0px)`;
-    return;
-  }
-
-  let images = document.querySelector("#list");
-  images.style.transform = `translateX(-${Slide * 650}px)`;
-
-  if (Slide === imageCount.length - 1) {
-    Slide = 0;
-  } else {
-    Slide++;
-  }
-}
-
-setInterval(() => {
-  startSlider();
-}, 4000);
-
-// slider 2
-let activeSlide = 0;
-function startSlider2() {
-  let imageCount = document.querySelectorAll(".slider2 ul li").length;
-  let images = document.querySelector(".slider2 ul");
-  console.log();
-  if (imageCount.length === 0) {
-    // imageCount = document.querySelectorAll(".slider2 ul li").length;
-    images.style.transform = `translateX(0px)`;
-    return;
-  }
-
-  images.style.transform = `translateX(-${activeSlide * 650}px)`;
-  if (activeSlide === imageCount - 1) {
-    activeSlide = 0;
-  } else {
-    activeSlide++;
-  }
-}
-
-setInterval(() => {
-  startSlider2();
-}, 4000);
+}, 4500);
 
 
 
